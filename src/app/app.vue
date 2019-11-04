c73efba5bd695afbe3587267e7afd21875414023
Leonid Kim <e_leokim@yahoo.com>
2019-11-03 19:33:53 -0800

    initial configuration

---
+++ src/app/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        Hello, world
+    </div>
+</template>
+
+<script lang="ts">
+    import { Vue, Component } from 'vue-property-decorator';
+
+    @Component
+    export default class App extends Vue {
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .card {
+        color: blue;
+    }
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,64 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: '/\\.s[ac]ss$/i',
-                use: [
-                    process.env.NODE_ENV !== 'production'
-                        ? 'style-loader'
-                        : MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true,
+module.exports = (env, argv) => {
+    return {
+        entry: './src/index.ts',
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.vue$/,
+                    loader: 'vue-loader',
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.ts$/,
+                    exclude: /node_modules/,
+                    use: ['babel-loader'],
+                },
+                {
+                    test: /\.(scss|css)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        argv.mode !== 'production'
+                            ? 'style-loader'
+                            : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                            },
                         },
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: true,
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true,
+                            },
                         },
-                    },
-                ],
-            },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new MiniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css',
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                template: path.resolve(__dirname, 'src/index.html'),
+            }),
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-        }),
-    ],
+    };
 };
